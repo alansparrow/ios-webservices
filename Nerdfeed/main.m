f07ef8cdf0816617f96f354d506bed126a675651
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-12-31 11:18:34 +0900

    Initial Commit

---
+++ Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by Alan Sparrow on 12/31/13.
+//  Copyright (c) 2013 Alan Sparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NerdfeedAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NerdfeedAppDelegate class]));
+    }
+}
